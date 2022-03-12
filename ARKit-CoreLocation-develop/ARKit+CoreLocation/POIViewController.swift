2fc631a4103f89816b3509c3994a6647f9c67c88
Kyle Wong <kylewong@169-231-158-68.wireless.ucsb.edu>
2022-03-12 14:31:41 -0800

    removed unnecessary stuff

--- ARKit-CoreLocation-develop/ARKit+CoreLocation/POIViewController.swift
+++ ARKit-CoreLocation-develop/ARKit+CoreLocation/POIViewController.swift
@@ -308,7 +308,7 @@ extension POIViewController {
         
         print("---------------")
         
-        var distance = curLocation?.distance(from: location)
+        let distance = curLocation?.distance(from: location)
         
         print(calculateHeading(curLocation: curLocation!, targetLocation: location))
     
@@ -363,8 +363,6 @@ extension POIViewController {
 
             self.modelNode = arcNode
 
-            let (minBox, maxBox) = self.modelNode.boundingBox
-//            self.modelNode.pivot = SCNMatrix4MakeTranslation(0, (maxBox.y - minBox.y) / 2, 0)
 //            arcNode.eulerAngles.y = (.pi/2)
 
             let rotation = SCNMatrix4MakeRotation(Float(bearing + 90).toRadians(), 0, 1, 0)
@@ -387,85 +385,6 @@ extension POIViewController {
         
         
     }
-    
-    func positionModel(_ location: CLLocation) {
-        // Rotate node
-        
-        self.modelNode.transform = rotateNode(Float(-1 * (self.heading - 180).toRadians()), self.originalTransform)
-            
-        // Translate node
-        self.modelNode.position = translateNode(location)
-            
-        // Scale node
-        self.modelNode.scale = scaleNode(location)
-    }
-    
-    func rotateNode(_ angleInRadians: Float, _ transform: SCNMatrix4) -> SCNMatrix4 {
-        let rotation = SCNMatrix4MakeRotation(angleInRadians, 0, 1, 0)
-        return SCNMatrix4Mult(transform, rotation)
-    }
-
-    func scaleNode (_ location: CLLocation) -> SCNVector3 {
-        let scale = min( max( Float(1000/distance), 0.0005 ), 0.001 )
-        return SCNVector3(x: scale, y: scale, z: scale)
-    }
-    
-    func translateNode (_ location: CLLocation) -> SCNVector3 {
-        let locationTransform =
-            transformMatrix(matrix_identity_float4x4, userLocation, location)
-        return positionFromTransform(locationTransform)
-    }
-    
-    func positionFromTransform(_ transform: simd_float4x4) -> SCNVector3 {
-        return SCNVector3Make(
-            transform.columns.3.x, transform.columns.3.y, transform.columns.3.z
-        )
-    }
-
-    func transformMatrix(_ matrix: simd_float4x4, _ originLocation: CLLocation, _ driverLocation: CLLocation) -> simd_float4x4 {
-        let bearing = bearingBetweenLocations(userLocation, driverLocation)
-        let rotationMatrix = rotateAroundY(matrix_identity_float4x4, Float(bearing))
-            
-        let position = vector_float4(0.0, 0.0, -distance, 0.0)
-        let translationMatrix = getTranslationMatrix(matrix_identity_float4x4, position)
-            
-        let transformMatrix = simd_mul(rotationMatrix, translationMatrix)
-            
-        return simd_mul(matrix, transformMatrix)
-    }
-        
-    func getTranslationMatrix(_ matrix: simd_float4x4, _ translation : vector_float4) -> simd_float4x4 {
-        var matrix = matrix
-        matrix.columns.3 = translation
-        return matrix
-    }
-        
-    func rotateAroundY(_ matrix: simd_float4x4, _ degrees: Float) -> simd_float4x4 {
-        var matrix = matrix
-        
-        matrix.columns.0.x = cos(degrees)
-        matrix.columns.0.z = -sin(degrees)
-            
-        matrix.columns.2.x = sin(degrees)
-        matrix.columns.2.z = cos(degrees)
-        return matrix.inverse
-    }
-        
-    func bearingBetweenLocations(_ originLocation: CLLocation, _ driverLocation: CLLocation) -> Double {
-        let lat1 = originLocation.coordinate.latitude.toRadians()
-        let lon1 = originLocation.coordinate.longitude.toRadians()
-            
-        let lat2 = driverLocation.coordinate.latitude.toRadians()
-        let lon2 = driverLocation.coordinate.longitude.toRadians()
-            
-        let longitudeDiff = lon2 - lon1
-            
-        let y = sin(longitudeDiff) * cos(lat2);
-        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(longitudeDiff);
-            
-        return atan2(y, x)
-    }
-    
     
     /// Builds the location annotations for a few random objects, scattered across the country
     ///
